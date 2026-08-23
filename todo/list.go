8179b770bfa697677ac878ecde6eadc95fa1d124
fruz333 <vasabadik@gmail.com>
2026-08-23 20:04:50 +0600

    2 commit

--- todo/list.go
+++ todo/list.go
@@ -9,3 +9,41 @@ func NewList() *list {
 		tasks: make(map[string]task),
 	}
 }
+
+func (l *list) addTask(task task) {
+	// создает новую задачу
+	l.tasks[task.title] = task
+}
+
+func (l *list) listTasks() map[string]task {
+	// выводит все задачи
+	return l.tasks
+}
+
+func (l *list) delTask(title string) string {
+	// удаляет задачу по заголовку
+	_, ok := l.tasks[title]
+	if !ok {
+		return errorNotFound
+	}
+
+	delete(l.tasks, title)
+
+	return ""
+}
+
+func (l *list) doneTask(title string) string {
+	// меняет статус выполнения и создает время выполнения по заголовку задачи
+	task, ok := l.tasks[title]
+	if !ok {
+		return errorNotFound
+	}
+	
+	task.done()
+
+	l.tasks[title] = task   // обновляем задачу
+
+	return ""
+}
+
+
--- todo/task.go
+++ todo/task.go
@@ -3,6 +3,7 @@ package todo
 import "time"
 
 type task struct {
+	// структура задачи
 	title  string
 	text   string
 	isDone bool
@@ -12,6 +13,7 @@ type task struct {
 }
 
 func NewTask(title string, text string) task {
+	// конструктор задачи 
 	return task{
 		title:  title,
 		text:   text,
@@ -20,4 +22,12 @@ func NewTask(title string, text string) task {
 		createdAt: time.Now(),
 		doneAt:    nil,
 	}
+}
+
+func (t *task) done() {
+	// меняет статус и добавляет время выполнения задачи	
+	t.isDone = true
+
+	now := time.Now()
+	t.doneAt = &now  
 }
